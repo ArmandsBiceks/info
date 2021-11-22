2c1ff17ca7a169b94d02910f5ab755a45c98d51d
Armands Bičeks <90174764+ArmandsBiceks@users.noreply.github.com>
2021-11-22 08:10:34 +0000

    .

---
+++ Uzdevums.py
@@ -0,0 +1,29 @@
+klients = input('ievadi savu vardu un uzvardu:')
+veltijums = input('raksti savu veltijumu:')
+izmers = input('ladites izmeri:' )
+materials = input('materiala cena:')
+
+class Rekins:
+  def _init_(self, klients, veltijums, izmers, materials):
+    self.klients = klients
+    self.veltijums = veltijums
+    self.izmers = izmers
+    self.materials = materials
+    
+ self.veltijums = len(self.veltijums)
+ 
+
+
+def izdrukat(self):
+  pass
+
+def aprekins(self):
+ darba_samaksa = 15
+ PVN = 21
+ produkta_cena = (veltījuma teksta garums simbolos) * 1.2 +
+ (platums/100 * garums/100 * augstums/100)/ 3 * materiala_cena
+ PVN_summa = (produkta_cena + darba_samaksa)*PVN/100
+ rekina_summa = (produkta_cena + darba_samaksa) + PVN_summa
+
+
+a = rekins(klients, izmers, veltijums, izmers)
