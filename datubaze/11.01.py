6a18d98aab7892adfb5da859c1224530a8107540
Armands Bičeks <90174764+ArmandsBiceks@users.noreply.github.com>
2022-01-11 07:20:06 +0000

    .

---
+++ datubaze/11.01.py
@@ -0,0 +1,19 @@
+from replit import db
+
+def galvena_izvele():
+  print(sakums)
+  izvele=input('izveleta darbiba')
+
+  if izvele =='1':
+    kontaktu_piev()
+  elif izvele =='2':
+    kont_atrasana()
+  else: 
+    print('nav tādas, mēģini velreiz')
+
+while True:
+  galvena_izvele()
+  input('\nNospiediet Enter, lai turpinātu\n\n')
+
+
+
